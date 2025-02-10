472c7f76f3130147aed2e84404fbc859c912ce5b
wendala <Gonzalez-jesus@correo.unimet.edu.ve>
2025-02-09 20:56:04 -0400

    Guardar los procesos como SCV
    Probar en main guardar los procesos como SCV

--- src/main/java/main/Proyecto1.java
+++ src/main/java/main/Proyecto1.java
@@ -4,6 +4,10 @@
 
 package main;
 
+import classes.ProcessImage;
+import classes.ProcessImageCSV;
+import primitivas.List;
+
 /**
  *
  * @author DELL
@@ -13,5 +17,14 @@ public class Proyecto1 {
     public static void main(String[] args) {
         W1 w1 = new W1();
         w1.setVisible(true);
+        
+        List<ProcessImage> processes = new List<>();
+
+        // Crear procesos de ejemplo y agregarlos a la lista
+        processes.appendLast(new ProcessImage(null, "System", 1, "Running", "Process1", 100, 200, 50));
+        processes.appendLast(new ProcessImage(null, "User", 2, "Waiting", "Process2", 150, 250, 30));
+
+        // Guardar en CSV
+        ProcessImageCSV.saveProcessesToCSV(processes, "procesos.csv");
     }
 }
